dbfc91477fc1386441bd89ac8d2c421faf26f289
Eddie-Ward <edwardliew1998@gmail.com>
2023-07-24 23:36:42 -0700

    Complete 1601: Maximum transfer requests

--- daily/1601-max-transfer-requests.cpp
+++ daily/1601-max-transfer-requests.cpp
@@ -15,11 +15,18 @@ class Solution {
 
     int backtrack(const vector<vector<int>>& requests, vector<int>& bal, int index) {
         if (index == requests.size()) {
-            if (all_of(bal.begin(), bal.end(), [](int i) { i == 0; })) {
+            if (all_of(bal.begin(), bal.end(), [](int i) { return i == 0; })) {
                 return 0;
             } else {
                 return INT_MIN;
             }
+        } else {
+            --bal[requests[index][0]];
+            ++bal[requests[index][1]];
+            int total = 1 + backtrack(requests, bal, index + 1);
+            ++bal[requests[index][0]];
+            --bal[requests[index][1]];
+            return max(total, backtrack(requests, bal, index + 1));
         }
     }
 };
