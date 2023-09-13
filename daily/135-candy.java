69d53ee771b6a91ea8482399ef70c995a2f07d17
Eddie-Ward <edwardliew1998@gmail.com>
2023-09-13 07:02:31 -0700

    Complete 135: Candy

---
+++ daily/135-candy.java
@@ -0,0 +1,30 @@
+package daily;
+
+import java.util.Arrays;
+
+class Solution {
+    public int candy(int[] ratings) {
+        int[] candy = new int[ratings.length];
+        Arrays.fill(candy, 1);
+
+        for (int i = 1; i < ratings.length; ++i) {
+            if (ratings[i] > ratings[i - 1]) {
+                candy[i] = candy[i - 1] + 1;
+            }
+        }
+
+        for (int i = ratings.length - 2; i >= 0; --i) {
+            if (ratings[i] > ratings[i + 1]) {
+                candy[i] = Math.max(candy[i], candy[i + 1] + 1);
+            }
+        }
+
+        int total = 0;
+
+        for (int num : candy) {
+            total += num;
+        }
+
+        return total;
+    }
+}
