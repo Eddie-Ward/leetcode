4325413ab8a7b4778511843a999038b5b1d4a82d
Eddie-Ward <edwardliew1998@gmail.com>
2023-12-14 18:29:56 -0800

    Complete 1436: Destination City

---
+++ daily/1436-destination-city.java
@@ -0,0 +1,19 @@
+package daily;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        HashSet<String> citySet = new HashSet<>();
+
+        for (List<String> cities : paths) {
+            citySet.add(cities.get(1));
+        }
+        for (List<String> cities : paths) {
+            citySet.remove(cities.get(0));
+        }
+        return citySet.iterator().next();
+    }
+}
